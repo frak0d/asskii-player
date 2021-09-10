99d81a2461273accb508fe7b9323d51c4c892bf1
frakod <frakod@pm.me>
2021-09-10 13:10:06 +0530

    wip

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <cmath>
 #include <cstdio>
+#include <string>
+#include <format>
+#include <cstdint>
 #include <cstdlib>
 #include <iostream>
+#include <filesystem>
 
 using namespace std;
 
@@ -19,7 +23,18 @@ void clear()
 	#endif
 }
 
+void save_frames(string& ffpath, string& filepath, uint32_t start, uint32_t end)
+{
+	string command = format("{0} -i {1} -vf select=\'between(n\,{2}\,{3})\' -vsync 0 -start_number {2} frames%d.png",
+							ffpath, filepath, start, end);
+	system(command.c_str());
+}
+
 int main(int argc, char const *argv[])
 {
+	string ff = "./ffmpeg/ffmpeg.exe";
+	string fl = "./ffmpeg/test.3gp";
+
+	save_frames(ff, fl, 20, 50);
 	return 0;
 }
