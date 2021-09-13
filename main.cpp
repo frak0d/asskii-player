4ad6b89c83e7eba75c72dcf85837d709dc40eae5
frakod <frakod@pm.me>
2021-09-13 12:42:45 +0530

    switched to cmake build system

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@
 #include <filesystem>
 
 using namespace std;
+namespace fs = std::filesystem;
 
 int WIDTH;
 int HEIGHT;
 
-void clear()
+void ClearScreen()
 {
 	#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
 		system("clear");
@@ -23,18 +24,50 @@ void clear()
 	#endif
 }
 
-void save_frames(string& ffpath, string& filepath, uint32_t start, uint32_t end)
+void DeleteFile(const string& filename)
 {
-	string command = format("{0} -i {1} -vf select=\'between(n\,{2}\,{3})\' -vsync 0 -start_number {2} frames%d.png",
-							ffpath, filepath, start, end);
+	#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+		system(format("rm \"{}\"", filename).c_str());
+	#endif
+
+	#if defined(_WIN32) || defined(_WIN64)
+		system(format("del \"{}\"", filename).c_str());
+	#endif
+}
+
+void DeleteFolder(const string& foldername)
+{
+	#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+		system(format("rm -r \"{}\"", foldername).c_str());
+	#endif
+
+	#if defined(_WIN32) || defined(_WIN64)
+		system(format("rmdir \"{}\"", foldername).c_str());
+	#endif
+}
+
+void save_frames(const string& ffpath, const string& filepath, const uint32_t start_num, const uint32_t end_num)
+{
+	string command = format("{0} -i {1} -vf select='between(n,{2},{3})' -vsync 0 -start_number {2} frame-%03d.png",
+							ffpath, filepath, start_num, end_num);
 	system(command.c_str());
 }
 
-int main(int argc, char const *argv[])
+void delete_frames(const string& folderpath, const uint32_t start_num, const uint32_t end_num)
 {
-	string ff = "./ffmpeg/ffmpeg.exe";
-	string fl = "./ffmpeg/test.3gp";
+	for (int i = start_num; i <= end_num; ++i)
+	{
+		DeleteFile(format("{}/frame-{:3d}", folderpath, i));
+	}
+}
 
-	save_frames(ff, fl, 20, 50);
+int main(int argc, const char* argv[])
+{
+	string ff = "ffmpeg.exe";
+	string fl = "test.3gp";
+
+	auto self_path = fs::path(argv[0]);
+	cout << self_path;
+	//save_frames(ff, fl, 20, 50);
 	return 0;
 }
