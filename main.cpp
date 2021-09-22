ed5712ddba2a2371c8534026a4c2caa3770d38f3
frakod <frakod@pm.me>
2021-09-22 07:01:31 +0000

    it works! kinda

--- main.cpp
+++ main.cpp
@@ -10,11 +10,10 @@
 
 using namespace std;
 
-uint WIDTH  = 80;
-uint HEIGHT = 50;
+uint WIDTH  = 60;
+uint HEIGHT = 40;
 
-int shades = 5;
-char shade_list[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+char shade_list[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 
 string QuoteShellArg(const string& arg)
 {
@@ -23,7 +22,7 @@ string QuoteShellArg(const string& arg)
 	ret.reserve(arg.length()+10); // ¯\_(ツ)_/¯ should avoid realloc in most cases
 	for(size_t i=0;i<arg.length();++i)
 	{
-		if(arg[i]=='\00')
+		if(arg[i]=='\0')
 			throw runtime_error("argument contains null bytes, it is impossible to escape null bytes on unix!");
 		else if(arg[i]=='\'')
 			ret+="'\\''";
@@ -58,8 +57,9 @@ void ClearScreen()
 struct config
 {
 	string vid_path;
+	uint8_t shades = 32;
 	bool color = false;
-	bool blocks = false;
+	char block = false;
 };
 
 config ArgumentParser(int argc, const char* argv[])
@@ -69,11 +69,11 @@ config ArgumentParser(int argc, const char* argv[])
 		cout << "Usage :-\n"
 			 << "./asski-player [video path] <optional arguments>\n\n"
 			 << "--color   -->  Display the Video in 24-bit Colors\n"
-			 << "--blocks  -->  Use Filled Blocks instead of Ascii Symbols\n"
-			 << "              (Requires --color, --color will be enabled)\n" << endl;
+			 << "--block   -->  Specify the character to use as pixel in Color Mode\n"
+			 << "               (it will be ignored when not using color)\n" << endl;
 		exit(-1);
 	}
-	else if (string(argv[1]) == "--color" || string(argv[1]) == "--blocks")
+	else if (string(argv[1]) == "--color" || string(argv[1]) == "--block")
 	{
 		puts("\033[91;1;3m==> Error : No Video File Specified !\033[m");
 		exit(-1);
@@ -85,8 +85,8 @@ config ArgumentParser(int argc, const char* argv[])
 	for (int i=2 ; i < argc ; i++)	//argv[0] is exe name, argv[1] is video name
 	{
 		tok = argv[i];
-		if 		(tok == "--color" ) cfg.color  = true;
-		else if (tok == "--blocks") cfg.blocks = true;
+		if 		(tok == "--color") cfg.color = true;
+		else if (tok == "--block") cfg.block = *argv[i+1];
 	}
     return cfg;
 }
@@ -96,11 +96,15 @@ int main(int argc, const char* argv[])
 	config cfg = ArgumentParser(argc, argv);
 
 	int pos = 0;
+	size_t rs;
+	float sf1 = 256/cfg.shades;
+	float sf2 = sizeof(shade_list) / cfg.shades;
 	uint8_t brightness = 0;
+	
 	uint8_t buf[WIDTH*3*HEIGHT];	// Save one image in buffer
 	
 	char test_cmd[500];
-	snprintf(test_cmd, 500, "ffmpeg -i %s -v quiet -vsync 0 -s %ux%u "
+	snprintf(test_cmd, 500, "ffmpeg -i %s -v quiet -vsync 1 -s %ux%u "
 							"-f image2pipe -vcodec rawvideo -pix_fmt rgb24 -",
 							cfg.vid_path.c_str(), WIDTH, HEIGHT);
 	
@@ -111,23 +115,30 @@ int main(int argc, const char* argv[])
 		exit(-1);
 	}
 
-	while (true)
-	{
-		size_t rs = fread(buf, 1, sizeof(buf), ffpipe);
+	do {
+		rs = fread(buf, 1, sizeof(buf), ffpipe);
     	//printf("\n\033[94;1;3m ++++++++ Read %lu Bytes +++++++++ \033[m\n", rs);
     	for (int y=0 ; y < HEIGHT ; ++y)
     	{
     		for (int x=0 ; x < WIDTH ; ++x)
     		{
     			++pos;
-    			brightness = (buf[3*pos-2]/16 + buf[3*pos-1]/16 + buf[3*pos]/16) / 3;
-    			printf("%c", shade_list[brightness*2]);
+    			if (!cfg.color)
+    			{
+    				brightness = (buf[3*pos-2]/sf1 + buf[3*pos-1]/sf1 + buf[3*pos]/sf1) / 3;
+    				printf(" %c", shade_list[(int)(brightness*sf2)]);
+    			}
+    			else
+    			{
+    				puts("Color Mode Not Yet Supported !!");
+    				exit(-2);
+    			}
     		}
     		puts("");	// Newline
     	}
     	pos = 0;
     	ClearScreen();
-	}
+	} while (rs == sizeof(buf));
     
     printf("\n\033[96;1m ==> Closing Pipe, %s\033[m\n", strerror(pclose(ffpipe)));
     return 0;
